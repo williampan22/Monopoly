7ae02565b12616535aa8807e3b780eb806e455b5
1861598 <1861598@01-19838.pusd.dom>
2022-03-29 10:14:42 -0700

    background

---
+++ Monopoly CSA/src/Background.java
@@ -0,0 +1,63 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Background{
+	
+	//add location attributes
+	private Image img; 	
+	private AffineTransform tx;
+
+	public Background(int x, int y) {
+		img = getImage("/imgs/bgCropped.png"); //load the image for Tree
+		tx = AffineTransform.getTranslateInstance(x, y );
+		init(x, y); 				//initialize the location of the image
+									//use your variables
+	}
+
+	
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		
+		
+		//call update to update the actualy picture location
+		update();
+		
+		
+		
+		
+		g2.drawImage(img, tx, null);
+		
+		
+
+	}
+	/* update the picture variable location */
+	private void update() {
+
+		
+		
+	}
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(2.7, 2.5);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Background.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+
+}
