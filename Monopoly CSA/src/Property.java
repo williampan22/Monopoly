1b967336194146645f540a7bda82c9b4ddbaa554
1861104 <1861104@01-19852.pusd.dom>
2022-03-30 12:01:42 -0700

    properties

---
+++ Monopoly CSA/src/Property.java
@@ -0,0 +1,31 @@
+
+public class Property {
+	
+	private String name; 
+	private int x; 
+	private int y; 
+	private String owner; 
+	private int price; //price to buy property
+	private int pay;  //price you pay when you land on property
+	
+	public Property(String name, int x, int y, String owner, int price, int pay) { 
+		this.name =  name; 
+		this.x = x; 
+		this.y = y; 
+		this.owner = owner; 
+		this.price = price; 
+		this.pay = pay; 
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+	}
+
+}
