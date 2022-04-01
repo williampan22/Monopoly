327b51d10923151c288cbab367f091ea41a71abb
1941455 <1941455@01-19813.pusd.dom>
2022-04-01 10:05:10 -0700

    dice

--- Monopoly CSA/src/dice.java
+++ Monopoly CSA/src/dice.java
@@ -9,11 +9,11 @@
 	    	  int diceX=(int)(Math.random()*6+1); 
 	    	  int diceY=(int)(Math.random()*6+1); 
 	    	  int sum1 = diceX + diceY; 
-	    	  System.out.println("Roll: total = " + sum1);  
+	    	  System.out.println("Roll : total = " + sum1);  
 	    	  if (sum1==2 || sum1==3 || sum1==12) { 
-	    	  System.out.println("Sorry with a " + sum1 + " You LOSE :("); 
+	    	  System.out.println("Sorry with or without a " + sum1 + " You LOSE :("); 
 	    	  break; 
-	    	  } else if(sum1==7 || sum1==11) {  
+	    	  } else if(sum1==7 || sum1 == 11) {  
 	    	  System.out.println("Woah!!! With a " + sum1 + " You WIN!!!!!!!"); 
 	    	  break;  
 	    	  } 
