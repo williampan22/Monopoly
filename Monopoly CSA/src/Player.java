9e7338422b56c576de1a5c0ea453f0a0561f457a
1861598 <1861598@01-19838.pusd.dom>
2022-05-16 11:22:57 -0700

    movement

--- Monopoly CSA/src/Player.java
+++ Monopoly CSA/src/Player.java
@@ -59,7 +59,7 @@ public class Player {
 			
 
 		}
-		/* update the picture variable locsations */
+		/* update the picture variable locssations */
 		private void update() {
 
 			tx.setToTranslation(x + bounceX, y + bounceY);
